d01a8fa878986843050fa12033d4c09750ae8409
StalkerE30 <mpower_e30@rambler.ru>
2022-10-12 06:58:07 +0500

    first commit

---
+++ src/main/kotlin/ru/netology/pusher/constants.kt
@@ -0,0 +1,4 @@
+package ru.netology.pusher
+
+const val dbUrl = ""
+const val token = "eldCLaaLQfqhw9osnapaNn:APA91bFcd4wrd801tQk3JAuBySB8zf4RZqu0dLbshXZ4vjl3_UDFhGCMs3cxo0bRPY8GiXxuN9iD7bXq4amwy4xyIZHQG5w8W1NXpKhcOw_-f8S7H-C9y1peqJ889Ut3Io71JKx8-7AT"
